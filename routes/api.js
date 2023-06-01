e6fb008642d69fb331bfd41483dfb35bcbcab07f
Stephen <127278005+slmov215@users.noreply.github.com>
2023-06-01 18:45:31 -0400

    tried to fix JSON err and create router and module

--- routes/api.js
+++ routes/api.js
@@ -10,10 +10,10 @@ const path = require("path");
 
 // GET route 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/notes.html'));
+  res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 router.get('/notes', (req, res) => {
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const fs = require("fs");
 const path = require("path");
 // Data-Base 
 const dataBase = require("./db/db.json")
+const uniqueID = require("./helpers/uuid")
+
+
 
 // Express function that creates new application 
 const app = express();
@@ -18,11 +21,58 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+
+// GET route 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+  
+  app.get('/notes', (req, res) => {
+    res.json(dataBase);
+  });
+  
+  // POST route, return new note, add to json, and return to client,
+  app.post('/notes', (req, res) => {
+    const { title, text } = req.body;
+    if (title && text) {
+      const newNote = {
+        title,
+        text,
+        id: uniqueID(),
+      };
+  
+      dataBase.push(newNote);
+  
+      let notesString = JSON.stringify(dataBase);
+  
+      fs.writeFile(`./db/db.json`, notesString, (err) =>
+        err ? console.error(err) : console.log(`Your new note, ${newNote.title}, has been added!`)
+      );
+  
+      // give success response or report errors
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      console.log(response);
+      // 201 = request has succeeded and has led to the creation of a resource
+      res.status(201).json(response);
+    } else {
+      // 500 = server-side error
+      res.status(500).json('Error in adding note');
+    }
+  });
+
+
 // Required router
-const apiRouter = require("./routes/api");
+// const apiRouter = require("./routes/api");
 
 // Use/link route to path
-app.use("/api", apiRouter);
+// app.use("/api", apiRouter);
 
 // App listener which starts the server
 app.listen(PORT, () =>
