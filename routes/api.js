561c6c0a79120a0f89a21c8945c8f4cb5973f1d3
Stephen <127278005+slmov215@users.noreply.github.com>
2023-06-02 00:17:53 -0400

    reroute routes

--- routes/api.js
+++ routes/api.js
@@ -2,26 +2,26 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
-let notesData = require("../db/db.json")
+let dataBase = require("../db/db.json")
 const uniqueID = require("../helpers/uuid")
 
 // Path import
 const path = require("path");
 
 // GET route 
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-router.get('/notes', (req, res) => {
-  res.json(notesData);
+router.get("/api/notes", (req, res) => {
+  res.json(dataBase);
 });
 
 // POST route, return new note, add to json, and return to client,
-router.post('/notes', (req, res) => {
+router.post("/api/notes", (req, res) => {
   const { title, text } = req.body;
   if (title && text) {
     const newNote = {
@@ -30,9 +30,9 @@ router.post('/notes', (req, res) => {
       id: uniqueID(),
     };
 
-    notesData.push(newNote);
+    dataBase.push(newNote);
 
-    let notesString = JSON.stringify(notesData);
+    let notesString = JSON.stringify(dataBase);
 
     fs.writeFile(`./db/db.json`, notesString, (err) =>
       err ? console.error(err) : console.log(`Your new note, ${newNote.title}, has been added!`)
@@ -52,4 +52,5 @@ router.post('/notes', (req, res) => {
     res.status(500).json('Error in adding note');
   }
 });
+
 module.exports = router;
--- server.js
+++ server.js
@@ -22,57 +22,57 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
-// GET route 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-  });
-  app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-  });
+// // GET route 
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "/public/index.html"));
+//   });
+//   app.get("/notes", (req, res) => {
+//     res.sendFile(path.join(__dirname, "/public/notes.html"));
+//   });
   
-  app.get('/notes', (req, res) => {
-    res.json(dataBase);
-  });
+//   app.get("/api/notes", (req, res) => {
+//     res.json(dataBase);
+//   });
   
-  // POST route, return new note, add to json, and return to client,
-  app.post('/notes', (req, res) => {
-    const { title, text } = req.body;
-    if (title && text) {
-      const newNote = {
-        title,
-        text,
-        id: uniqueID(),
-      };
+//   // POST route, return new note, add to json, and return to client,
+//   app.post("/api/notes", (req, res) => {
+//     const { title, text } = req.body;
+//     if (title && text) {
+//       const newNote = {
+//         title,
+//         text,
+//         id: uniqueID(),
+//       };
   
-      dataBase.push(newNote);
+//       dataBase.push(newNote);
   
-      let notesString = JSON.stringify(dataBase);
+//       let notesString = JSON.stringify(dataBase);
   
-      fs.writeFile(`./db/db.json`, notesString, (err) =>
-        err ? console.error(err) : console.log(`Your new note, ${newNote.title}, has been added!`)
-      );
+//       fs.writeFile(`./db/db.json`, notesString, (err) =>
+//         err ? console.error(err) : console.log(`Your new note, ${newNote.title}, has been added!`)
+//       );
   
-      // give success response or report errors
-      const response = {
-        status: 'success',
-        body: newNote,
-      };
+//       // give success response or report errors
+//       const response = {
+//         status: 'success',
+//         body: newNote,
+//       };
   
-      console.log(response);
-      // 201 = request has succeeded and has led to the creation of a resource
-      res.status(201).json(response);
-    } else {
-      // 500 = server-side error
-      res.status(500).json('Error in adding note');
-    }
-  });
+//       console.log(response);
+//       // 201 = request has succeeded and has led to the creation of a resource
+//       res.status(201).json(response);
+//     } else {
+//       // 500 = server-side error
+//       res.status(500).json('Error in adding note');
+//     }
+//   });
 
 
 // Required router
-// const apiRouter = require("./routes/api");
+const apiRouter = require("./routes/api");
 
 // Use/link route to path
-// app.use("/api", apiRouter);
+app.use("/api", apiRouter);
 
 // App listener which starts the server
 app.listen(PORT, () =>
